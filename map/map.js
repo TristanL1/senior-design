bc00137078be4c1aefcd3dd6688a8c7612911046
Alfredo Perry <alfredoperry14@gmail.com>
2023-01-21 21:46:55 -0800

    popup working but does not reformat to phone size

---
+++ map/map.js
@@ -0,0 +1,9 @@
+window.onload = function open(event){
+        document.querySelector(".popup").style.display = "block";
+
+        const hideButton = document.querySelector("#close");
+        const myElement = document.querySelector(".popup")
+    hideButton.addEventListener("click", function() {
+        myElement.style.display = "none";
+    });
+};
