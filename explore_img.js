bbd1ad89579dfad7d53f8dd39ed2cb2591097851
Tristan Limawan <tlimawan@gmail.com>
2023-05-02 17:51:56 -0700

    removed qr code from explore_img

--- explore_img.js
+++ explore_img.js
@@ -32,11 +32,9 @@ function initImgMarkers() {
         updateTrackingInfo(e.target.getAttribute("data-info-pane"), e.target.getAttribute("data-marker-name"), true);
         var quiz = document.getElementById("quiz");
         var info = document.getElementById("info");
-        var qrcode = document.getElementById("qrcode");
 
         quiz.disabled = false;
         info.disabled = false;
-        qrcode.disabled = false;
 
         if (e.target.getAttribute("id") == "nft-marker1") {
             quiz.onclick = function () {
@@ -84,9 +82,7 @@ function initImgMarkers() {
         updateTrackingInfo(e.target.getAttribute("data-info-pane"), e.target.getAttribute("data-marker-name"), false);
         var quiz = document.getElementById("quiz");
         var info = document.getElementById("info");
-        var qrcode = document.getElementById("qrcode");
         quiz.disabled = true;
         info.disabled = true;
-        qrcode.disabled = true;
     });
 }
